76726a257135d8652a5252addf15f316ed7ab736
Eric Capuano <capuanoe@gmail.com>
2015-05-01 20:13:55 -0500

    Update and rename initialize_copter.sh to reset_copter.sh
    added absolute path
    does not need sudo
    
    script starts the sim with a stock copter. user should ctrl-c once the sim begins, after the build

--- initialize_copter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-cd ~/ardupilot/ArduCopter  #move to appropriate vehicle directory
-
-make configure  #setup the build configuration
-
-sim_vehicle.sh -w   # runs sim script with option to wipe the virtual eeprom, needs testing, not sure if can run without absolute path
-
