04456007c34565407d117933c2a6253f1248e6e7
Eric Capuano <capuanoe@gmail.com>
2015-05-01 20:36:57 -0500

    Update start_sim.sh
    added absolute path
    fixed echo
    fixed home paths

--- start_sim.sh
+++ start_sim.sh
@@ -4,14 +4,15 @@ simframe=""
 frameshort=""
 subframe="" #not used, but could be for X + configuration
 framepath=""
-installpath="~/ardupilot"
+installpath="/ardupilot" #relative from $HOME
 #GCS1="192.168.1.12" #not using yet, sim already outs to localhost
 numcores=4   #need to add an autodetect method or user prompt to ensure core count is correct
 
 
 chooseframe()
 {
-SS1='Welcome to APM Simulator. Please select an aircraft:'
+clear # declutter
+echo 'Welcome to APM Simulator. Please select an aircraft:'
 options=("Quadcopter" "Octocopter" "Plane" "Quit")
 select opt in "${options[@]}"
 do
@@ -20,21 +21,21 @@ do
       echo "you chose Quadcopter"
       simframe="Quadcopter"
       frameshort="Quad"
-      framepath="$installpath/ArduCopter/"
+      framepath="/ArduCopter/"
       startsim
       ;;
     "Octocopter")
       echo "you chose Octocopter"
       simframe="Octocopter"
       frameshort="Octo"
-      framepath="$installpath/ArduCopter/"
+      framepath="/ArduCopter/"
       startsim
       ;;
     "Plane")
       echo "you chose Plane"
       simframe="Plane"
       frameshort="Plane"
-      framepath="$installpath/ArduPlane/"
+      framepath="/ArduPlane/"
       startsim
       ;;
     "Quit")
@@ -49,8 +50,13 @@ done
 startsim()
 {
 
-cd $framepath  #change to vehicle directory
+#debug
+#cd $installpath$framepath
+#echo $installpath$framepath
 
-sim_vehicle.sh -v Test$frameshort -j $numcores #--out $GCS1  #could still use -f X and -N options
+cd $HOME$installpath$framepath  #change to vehicle directory
+~/ardupilot/Tools/autotest/sim_vehicle.sh -v Test$frameshort -j $numcores #--out $GCS1  #could still use -f X and -N options
 
 }
+
+chooseframe
