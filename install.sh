2f71bcd68e274e672d2d2dacd3d91051931bbad8
Eric Capuano <capuanoe@gmail.com>
2015-05-01 20:04:28 -0500

    Update install.sh
    made some changes.
    must be run w/sudo

--- install.sh
+++ install.sh
@@ -2,17 +2,16 @@
 
 apt-get update && apt-get upgrade -y
 
+# download a bunch of dependencies and fun stuff
+apt-get install -y libtool automake autoconf libexpat1-dev python-matplotlib python-serial python-wxgtk2.8 python-lxml 
+apt-get install -y python-scipy python-opencv ccache gawk git python-pip python-pexpect
+
+pip install pymavlink MAVProxy  # installs MAVProxy via python package mgr
+
 cd ~   #sitl should be run from home directory
 
 git clone git://github.com/diydrones/ardupilot.git  #download official ardupilot code 
-
 git clone git://github.com/tridge/jsbsim.git  #download realistic sim software for fixed wing / plane
-
-# download a bunch of dependencies and fun stuff
-apt-get install libtool automake autoconf libexpat1-dev python-matplotlib python-serial python-wxgtk2.8 python-lxml 
-apt-get install python-scipy python-opencv ccache gawk git python-pip python-pexpect
-
-pip install pymavlink MAVProxy  # installs MAVProxy via python package mgr
 
 export PATH=$PATH:$HOME/jsbsim/src
 export PATH=$PATH:$HOME/ardupilot/Tools/autotest
